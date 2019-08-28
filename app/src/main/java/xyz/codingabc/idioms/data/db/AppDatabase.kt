aa3646b63f1e8ebd46d104319e5d95e648e60bee
h4rdw1r3 <vinaugust6@gmail.com>
2019-08-28 16:11:59 +0530

    add method for copying db from assets
    copy db from assets to the app database and migrate it for room

--- app/src/main/java/xyz/codingabc/idioms/data/db/AppDatabase.kt
+++ app/src/main/java/xyz/codingabc/idioms/data/db/AppDatabase.kt
@@ -1,69 +1,72 @@
 package xyz.codingabc.idioms.data.db
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import xyz.codingabc.idioms.data.dao.IdiomDao
-import xyz.codingabc.idioms.data.util.ioThread
 import xyz.codingabc.idioms.data.model.Idiom
+import java.io.FileOutputStream
+import java.io.IOException
 
-@Database(entities = [Idiom::class], version = 1 , exportSchema = false)
+@Database(entities = [Idiom::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun idiomDao(): IdiomDao
 
     companion object {
+
+        private const val DATABASE_NAME = "idioms.db"
+
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
         fun getInstance(context: Context): AppDatabase =
             INSTANCE ?: synchronized(this) {
                 INSTANCE
-                    ?: buildDatabase(context).also { INSTANCE = it}
+                    ?: buildDatabase(context).also { INSTANCE = it }
             }
 
 
-        private fun buildDatabase(context: Context) =
-            Room.databaseBuilder(
+        private fun buildDatabase(context: Context): AppDatabase {
+
+            copyAttachedDatabase(context)
+
+            return Room.databaseBuilder(
                 context.applicationContext,
                 AppDatabase::class.java,
-                "idioms_db"
-            ).addCallback(object: Callback() {
-                override fun onCreate(db: SupportSQLiteDatabase) {
-                    super.onCreate(db)
-                    ioThread {
-                        getInstance(context)
-                            .idiomDao()
-                            .insertData(PREPOPULATE_DATA)
-                    }
+                DATABASE_NAME
+            ).addMigrations(object : Migration(1, 2) {
+                override fun migrate(database: SupportSQLiteDatabase) {
+                    // do something with migration here
                 }
             }).build()
+        }
 
-        /*fun getDatabase(context: Context): AppDatabase {
-            return INSTANCE ?: synchronized(this) {
-                // Create database here
-                val instance = Room.inMemoryDatabaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java
-                    "idioms_database"
-                ).build()
-                    INSTANCE = instance
-                instance
+        private fun copyAttachedDatabase(context: Context) {
+            val dbPath = context.getDatabasePath(DATABASE_NAME)
+
+            // If the database already exists, return
+            if (dbPath.exists()) return
+
+            // make sure we have a path to the file
+            dbPath.parentFile.mkdirs()
+
+            // try to copy the file
+            try {
+                val inputStream = context.assets.open("databases/$DATABASE_NAME")
+                val output = FileOutputStream(dbPath)
+
+                inputStream.copyTo(output, 8192)
+                inputStream.close()
+
+                output.flush()
+                output.close()
+            } catch (e: IOException) {
+                Log.d("AppDatabase", "Failed to open file", e)
             }
-        }*/
-
-        val PREPOPULATE_DATA = listOf(
-            Idiom("1", "some idiom text", "some idiom meaning"),
-            Idiom("2", "some idiom text", "some idiom meaning"),
-            Idiom("3", "some idiom text", "some idiom meaning"),
-            Idiom("4", "some idiom text", "some idiom meaning"),
-            Idiom("5", "some idiom text", "some idiom meaning"),
-            Idiom("6", "some idiom text", "some idiom meaning"),
-            Idiom("7", "some idiom text", "some idiom meaning"),
-            Idiom("8", "some idiom text", "some idiom meaning"),
-            Idiom("9", "some idiom text", "some idiom meaning"),
-            Idiom("10", "some idiom text", "some idiom meaning")
-        )
+        }
     }
 }

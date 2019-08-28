1bbbf81ecd4eecd9ccab434dbb684eab923f9b24
h4rdw1r3 <vinaugust6@gmail.com>
2019-08-29 00:24:55 +0530

    add toolbar and setUpWithNavController

--- app/src/main/java/xyz/codingabc/idioms/MainActivity.kt
+++ app/src/main/java/xyz/codingabc/idioms/MainActivity.kt
@@ -1,12 +1,20 @@
 package xyz.codingabc.idioms
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.navHostFragment)
+        toolbar.setupWithNavController(navController, null)
     }
 }

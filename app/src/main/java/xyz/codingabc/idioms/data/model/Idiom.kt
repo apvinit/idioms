c5ade5d9692cd9b4e37df48c08cc8b4947d554f6
h4rdw1r3 <vinaugust6@gmail.com>
2019-08-28 15:57:19 +0530

    define table name for entity

--- app/src/main/java/xyz/codingabc/idioms/data/model/Idiom.kt
+++ app/src/main/java/xyz/codingabc/idioms/data/model/Idiom.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "idioms")
 data class Idiom(
     @PrimaryKey val _id: String,
     @ColumnInfo(name = "idiom") val text: String,

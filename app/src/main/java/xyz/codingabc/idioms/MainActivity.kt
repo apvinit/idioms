0b60336ae20a5394903ba7c4d9792e06340719a4
h4rdw1r3 <vinaugust6@gmail.com>
2019-08-31 16:47:55 +0530

    Integrate banner ads

--- app/src/main/java/xyz/codingabc/idioms/MainActivity.kt
+++ app/src/main/java/xyz/codingabc/idioms/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         navController = findNavController(R.id.navHostFragment)
         toolbar.setupWithNavController(navController, null)
         setSupportActionBar(toolbar)
+
+        MobileAds.initialize(this)
+
+        val adRequest = AdRequest.Builder().addTestDevice("BC8E628EEE8616E4D117EA3A610DD0C6").build()
+        adView.loadAd(adRequest)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {

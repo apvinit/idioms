81ca237af86e822b93d0344cc35a3af930c1e2d2
h4rdw1r3 <vinaugust6@gmail.com>
2019-09-12 14:54:52 +0530

    create setting fragment for preference page

---
+++ app/src/main/java/xyz/codingabc/idioms/fragments/SettingsFragment.kt
@@ -0,0 +1,24 @@
+package xyz.codingabc.idioms.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import xyz.codingabc.idioms.R
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return TextView(activity).apply {
+            setText(R.string.hello_blank_fragment)
+        }
+    }
+
+
+}

c4872ec3aedbc1dbc82ae3b7c5956d8d528d3cb0
h4rdw1r3 <vinaugust6@gmail.com>
2019-09-18 15:30:48 +0530

    create antonyms adapter

---
+++ app/src/main/java/xyz/codingabc/idioms/antonyms/AntonymListAdapter.kt
@@ -0,0 +1,66 @@
+package xyz.codingabc.idioms.antonyms
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textview.MaterialTextView
+import xyz.codingabc.idioms.R
+import xyz.codingabc.idioms.data.model.Antonym
+
+@Suppress("UNCHECKED_CAST")
+class AntonymListAdapter(private val antonyms: List<Antonym>) :
+    RecyclerView.Adapter<AntonymListAdapter.ViewHolder>(), Filterable {
+
+    private var antonymsFiltered: List<Antonym> = antonyms
+    private var searchKeyword = ""
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_antonym, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = antonymsFiltered.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bindAntonym(antonymsFiltered[position])
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val keyword = constraint.toString()
+                searchKeyword = keyword
+                antonymsFiltered = if (keyword.isEmpty()) antonyms else {
+                    val filteredList = mutableListOf<Antonym>()
+                    for (antonym in antonyms) {
+                        if (antonym.word.contains(keyword, true)) {
+                            filteredList.add(antonym)
+                        }
+                    }
+                    filteredList
+                }
+
+                val filterResults = FilterResults()
+                filterResults.values = antonymsFiltered
+                return filterResults
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                antonymsFiltered = results?.values as List<Antonym>
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val antonymWord = view.findViewById<MaterialTextView>(R.id.antonym_word)
+
+        fun bindAntonym(antonym: Antonym) {
+            antonymWord.text = antonym.word
+        }
+    }
+}

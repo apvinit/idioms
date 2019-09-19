2d879a036ec968ddd3aab681d080904e1a15cbc5
h4rdw1r3 <vinaugust6@gmail.com>
2019-09-19 16:34:35 +0530

    add pager to nav graph and handle preposition item click

--- app/src/main/java/xyz/codingabc/idioms/prepositions/PrepositionListAdapter.kt
+++ app/src/main/java/xyz/codingabc/idioms/prepositions/PrepositionListAdapter.kt
@@ -1,22 +1,28 @@
 package xyz.codingabc.idioms.prepositions
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textview.MaterialTextView
 import xyz.codingabc.idioms.R
 import xyz.codingabc.idioms.data.model.Preposition
 
-class PrepositionListAdapter(private val prepositions: List<Preposition>): RecyclerView.Adapter<PrepositionListAdapter.ViewHolder>(), Filterable {
+@Suppress("UNCHECKED_CAST")
+class PrepositionListAdapter(private val prepositions: List<Preposition>) :
+    RecyclerView.Adapter<PrepositionListAdapter.ViewHolder>(), Filterable {
 
     private var prepositionsFiltered: List<Preposition> = prepositions
     private var searchKeyword = ""
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_preposition, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_preposition, parent, false)
         return ViewHolder(view)
     }
 
@@ -53,11 +59,25 @@ class PrepositionListAdapter(private val prepositions: List<Preposition>): Recyc
         }
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         private val wordPreposition = view.findViewById<MaterialTextView>(R.id.word_preposition)
 
+
+        init {
+            view.findViewById<ConstraintLayout>(R.id.preposition_container).setOnClickListener {
+                val bundle = Bundle()
+                bundle.putInt("position", layoutPosition)
+                bundle.putString("keyword", searchKeyword)
+                it.findNavController()
+                    .navigate(
+                        R.id.action_destination_preposition_to_prepositionPager,
+                        bundle
+                    )
+            }
+        }
+
         fun bindPreposition(preposition: Preposition) {
-            wordPreposition.text = "${preposition.word} ${preposition.preposition}"
+            wordPreposition.text = view.context.resources.getString(R.string.word_preposition, preposition.word, preposition.preposition)
         }
     }
 }

621a6a83ee11bc048acfe6cb7d622b478f24c483
h4rdw1r3 <vinaugust6@gmail.com>
2019-09-19 14:23:29 +0530

    handle click on ows item

--- app/src/main/java/xyz/codingabc/idioms/onewordsubstitutions/OneWordSubstitutionListAdapter.kt
+++ app/src/main/java/xyz/codingabc/idioms/onewordsubstitutions/OneWordSubstitutionListAdapter.kt
@@ -1,10 +1,13 @@
 package xyz.codingabc.idioms.onewordsubstitutions
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textview.MaterialTextView
 import xyz.codingabc.idioms.R
@@ -59,6 +62,20 @@ class OneWordSubstitutionListAdapter(private val oneWordSubs: List<OneWordSubsti
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val owsWord = view.findViewById<MaterialTextView>(R.id.ows_word)
+
+        init {
+            view.findViewById<ConstraintLayout>(R.id.ows_container).setOnClickListener {
+                val bundle = Bundle()
+                bundle.putInt("position", layoutPosition)
+                bundle.putString("keyword", searchKeyword)
+                it.findNavController()
+                    .navigate(
+                        R.id.action_destination_one_word_substitution_to_oneWordSubsPager,
+                        bundle
+                    )
+            }
+        }
+
         fun bindOneWordSub(ows: OneWordSubstitution) {
             owsWord.text = ows.word
         }
